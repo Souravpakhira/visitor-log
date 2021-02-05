073a71ce4b320e60be3abb65de9e9e5d2c8bb6ec
Souravpakhira <pakhira55@gmail.com>
2021-02-05 20:51:06 +0530

    log

--- src/components/Log/Log.jsx
+++ src/components/Log/Log.jsx
@@ -1,9 +1,16 @@
 import React from 'react'
 
 const Log = () => {
+    let Lists=[];
+    Lists = JSON.parse(localStorage.getItem('Logs'))
+    console.log(Lists)
+
     return (
+
         <div>
-            Log
+            {Lists.map(list=>{
+                return <li>{list.Name}</li> 
+            })}
         </div>
     )
 }
